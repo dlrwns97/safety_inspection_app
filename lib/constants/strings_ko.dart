f10ce06d6b1a33bb79a7c28aaa0d749fd6c30455
dlrwns97 <dlrwns97@gmail.com>
2026-01-21 15:56:39 +0900

    슈미트해머(SH) 팝업(부재+최댓값/최솟값) 추가

--- lib/constants/strings_ko.dart
+++ lib/constants/strings_ko.dart
@@ -50,7 +50,7 @@ class StringsKo {
   static const defectCategoryOther = '기타 결함';
   static const equipmentCategory1 = '장비1';
   static const equipmentCategory2 = '철근배근간격';
-  static const equipmentCategory3 = '장비3';
+  static const equipmentCategory3 = '슈미트해머';
   static const equipmentCategory4 = '장비4';
   static const otherOptionLabel = '기타';
   static const otherTypeLabel = '기타 유형';
