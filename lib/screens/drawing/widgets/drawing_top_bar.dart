e4daf704a2d1cf06ed500345f64635c083ebbbf2
dlrwns97 <dlrwns97@gmail.com>
2026-01-22 16:29:37 +0900

    refactor drawing screen layout widgets

---
+++ lib/screens/drawing/widgets/drawing_top_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:safety_inspection_app/models/drawing_enums.dart';
+import 'package:safety_inspection_app/screens/drawing/widgets/tool_detail_row.dart';
+import 'package:safety_inspection_app/screens/drawing/widgets/tool_selection_row.dart';
+
+class DrawingTopBar extends StatelessWidget implements PreferredSizeWidget {
+  const DrawingTopBar({
+    super.key,
+    required this.mode,
+    required this.isToolSelectionMode,
+    required this.defectTabs,
+    required this.activeCategory,
+    required this.activeEquipmentCategory,
+    required this.onToggleMode,
+    required this.onBack,
+    required this.onAdd,
+    required this.onDefectSelected,
+    required this.onDefectLongPress,
+    required this.onEquipmentSelected,
+  });
+
+  static const double _toolBarHeight = 56.0;
+
+  final DrawMode mode;
+  final bool isToolSelectionMode;
+  final List<DefectCategory> defectTabs;
+  final DefectCategory? activeCategory;
+  final EquipmentCategory? activeEquipmentCategory;
+  final ValueChanged<DrawMode> onToggleMode;
+  final VoidCallback onBack;
+  final VoidCallback onAdd;
+  final ValueChanged<DefectCategory> onDefectSelected;
+  final ValueChanged<DefectCategory> onDefectLongPress;
+  final ValueChanged<EquipmentCategory> onEquipmentSelected;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(_toolBarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: isToolSelectionMode
+            ? ToolSelectionRow(
+                mode: mode,
+                onToggleMode: onToggleMode,
+              )
+            : ToolDetailRow(
+                mode: mode,
+                defectTabs: defectTabs,
+                activeCategory: activeCategory,
+                activeEquipmentCategory: activeEquipmentCategory,
+                onBack: onBack,
+                onAdd: onAdd,
+                onDefectSelected: onDefectSelected,
+                onDefectLongPress: onDefectLongPress,
+                onEquipmentSelected: onEquipmentSelected,
+              ),
+      ),
+    );
+  }
+}
