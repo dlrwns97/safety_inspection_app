76d589a6a40c5000109e0217d0f28c3703ae4ae4
dlrwns97 <dlrwns97@gmail.com>
2026-01-23 16:57:12 +0900

    Extract marker presenters

--- lib/screens/drawing/drawing_screen.dart
+++ lib/screens/drawing/drawing_screen.dart
@@ -28,6 +28,7 @@ import 'package:safety_inspection_app/screens/drawing/flows/drawing_dialogs_adap
 import 'package:safety_inspection_app/screens/drawing/flows/defect_marker_flow.dart';
 import 'package:safety_inspection_app/screens/drawing/flows/equipment_pack_d_flow.dart';
 import 'package:safety_inspection_app/screens/drawing/flows/equipment_updated_site_flow.dart';
+import 'package:safety_inspection_app/screens/drawing/flows/marker_presenters.dart';
 import 'package:safety_inspection_app/screens/drawing/widgets/canvas_marker_layer.dart';
 import 'package:safety_inspection_app/screens/drawing/widgets/drawing_local_parts.dart';
 import 'package:safety_inspection_app/screens/drawing/widgets/drawing_scaffold_body.dart';
@@ -409,7 +410,7 @@ class _DrawingScreenState extends State<DrawingScreen> {
     final equipmentCount = _site.equipmentMarkers
         .where((marker) => marker.category == _activeEquipmentCategory)
         .length;
-    final prefix = _equipmentLabelPrefix(_activeEquipmentCategory!);
+    final prefix = equipmentLabelPrefix(_activeEquipmentCategory!);
     final label = '$prefix${equipmentCount + 1}';
     final pendingMarker = EquipmentMarker(
       id: DateTime.now().microsecondsSinceEpoch.toString(),
@@ -427,7 +428,7 @@ class _DrawingScreenState extends State<DrawingScreen> {
       activeEquipmentCategory: _activeEquipmentCategory,
       pendingMarker: pendingMarker,
       prefix: prefix,
-      equipmentDisplayLabel: _equipmentDisplayLabel,
+      equipmentDisplayLabel: equipmentDisplayLabel,
       deflectionMemberOptions: DrawingDeflectionMemberOptions,
       showEquipmentDetailsDialog: _dialogs.equipmentDetails,
       showRebarSpacingDialog: (
@@ -515,8 +516,8 @@ class _DrawingScreenState extends State<DrawingScreen> {
     required double normalizedY,
   }) async {
     final nextIndices = {
-      'Lx': _nextSettlementIndex('Lx'),
-      'Ly': _nextSettlementIndex('Ly'),
+      'Lx': nextSettlementIndex(_site, 'Lx'),
+      'Ly': nextSettlementIndex(_site, 'Ly'),
     };
     final updatedSite = await createEquipment8IfConfirmed(
       context: context,
@@ -525,7 +526,7 @@ class _DrawingScreenState extends State<DrawingScreen> {
       normalizedX: normalizedX,
       normalizedY: normalizedY,
       nextIndexByDirection: nextIndices,
-      nextSettlementIndex: _nextSettlementIndex,
+      nextSettlementIndex: (direction) => nextSettlementIndex(_site, direction),
       showSettlementDialog: ({
         required baseTitle,
         required nextIndexByDirection,
@@ -657,9 +658,9 @@ class _DrawingScreenState extends State<DrawingScreen> {
     return _showDetailDialog(
       () => showDefectDetailsDialog(
         context: context,
-        title: _defectDialogTitle(defectCategory),
-        typeOptions: _defectTypeOptions(defectCategory),
-        causeOptions: _defectCauseOptions(defectCategory),
+        title: defectDialogTitle(defectCategory),
+        typeOptions: defectTypeOptions(defectCategory),
+        causeOptions: defectCauseOptions(defectCategory),
       ),
     );
   }
@@ -914,8 +915,8 @@ class _DrawingScreenState extends State<DrawingScreen> {
     }
 
     final lines = _selectedDefect != null
-        ? _defectPopupLines(_selectedDefect!)
-        : _equipmentPopupLines(_selectedEquipment!);
+        ? defectPopupLines(_selectedDefect!)
+        : equipmentPopupLines(_selectedEquipment!);
 
     final markerViewportPosition = MatrixUtils.transformPoint(
       _transformationController.value,
@@ -941,8 +942,8 @@ class _DrawingScreenState extends State<DrawingScreen> {
     }
 
     final lines = selectedDefect != null
-        ? _defectPopupLines(selectedDefect)
-        : _equipmentPopupLines(selectedEquipment!);
+        ? defectPopupLines(selectedDefect)
+        : equipmentPopupLines(selectedEquipment!);
 
     final normalizedX =
         selectedDefect?.normalizedX ?? selectedEquipment!.normalizedX;
@@ -998,123 +999,6 @@ class _DrawingScreenState extends State<DrawingScreen> {
     );
   }
 
-  List<String> _defectPopupLines(Defect defect) {
-    final details = defect.details;
-    return [
-      defect.label,
-      '${defect.category.label} / ${details.crackType}',
-      '${_formatNumber(details.widthMm)} / ${_formatNumber(details.lengthMm)}',
-      details.cause,
-    ];
-  }
-
-  List<String> _equipmentPopupLines(EquipmentMarker marker) {
-    List<String> baseLines(EquipmentMarker marker) =>
-        <String>[_equipmentDisplayLabel(marker)];
-    void addMemberTypeIfPresent(EquipmentMarker marker, List<String> lines) {
-      if (marker.memberType != null && marker.memberType!.isNotEmpty) {
-        lines.add(marker.memberType!);
-      }
-    }
-
-    final buildersByType = <String, List<String> Function(EquipmentMarker)>{
-      'F': (marker) {
-        final lines = baseLines(marker);
-        addMemberTypeIfPresent(marker, lines);
-        if (marker.numberText != null && marker.numberText!.isNotEmpty) {
-          lines.add('번호: ${marker.numberText}');
-        }
-        return lines;
-      },
-      'SH': (marker) {
-        final lines = baseLines(marker);
-        addMemberTypeIfPresent(marker, lines);
-        if (marker.maxValueText != null && marker.maxValueText!.isNotEmpty) {
-          lines.add('최댓값: ${marker.maxValueText}');
-        }
-        if (marker.minValueText != null && marker.minValueText!.isNotEmpty) {
-          lines.add('최솟값: ${marker.minValueText}');
-        }
-        return lines;
-      },
-      'Co': (marker) {
-        final lines = baseLines(marker);
-        addMemberTypeIfPresent(marker, lines);
-        if (marker.avgValueText != null && marker.avgValueText!.isNotEmpty) {
-          lines.add('평균값: ${marker.avgValueText}');
-        }
-        return lines;
-      },
-      'Ch': (marker) {
-        final lines = baseLines(marker);
-        addMemberTypeIfPresent(marker, lines);
-        if (marker.coverThicknessText != null &&
-            marker.coverThicknessText!.isNotEmpty) {
-          lines.add('피복두께: ${marker.coverThicknessText}');
-        }
-        if (marker.depthText != null && marker.depthText!.isNotEmpty) {
-          lines.add('깊이: ${marker.depthText}');
-        }
-        return lines;
-      },
-      'Tr': (marker) {
-        final lines = baseLines(marker);
-        if (marker.tiltDirection != null && marker.tiltDirection!.isNotEmpty) {
-          lines.add('방향: ${marker.tiltDirection}');
-        }
-        if (marker.displacementText != null &&
-            marker.displacementText!.isNotEmpty) {
-          lines.add('변위량: ${marker.displacementText}');
-        }
-        return lines;
-      },
-      'L': (marker) {
-        final lines = baseLines(marker);
-        addMemberTypeIfPresent(marker, lines);
-        if (marker.deflectionEndAText != null &&
-            marker.deflectionEndAText!.isNotEmpty) {
-          lines.add('A(단부): ${marker.deflectionEndAText}');
-        }
-        if (marker.deflectionMidBText != null &&
-            marker.deflectionMidBText!.isNotEmpty) {
-          lines.add('B(중앙): ${marker.deflectionMidBText}');
-        }
-        if (marker.deflectionEndCText != null &&
-            marker.deflectionEndCText!.isNotEmpty) {
-          lines.add('C(단부): ${marker.deflectionEndCText}');
-        }
-        return lines;
-      },
-    };
-    final builder = buildersByType[marker.equipmentTypeId];
-    if (builder != null && marker.equipmentTypeId != 'L') {
-      return builder(marker);
-    }
-    if (marker.category == EquipmentCategory.equipment8) {
-      final lines = <String>[_equipmentDisplayLabel(marker)];
-      final direction = _settlementDirection(marker);
-      if (direction != null && direction.isNotEmpty) {
-        lines.add('방향: $direction');
-      }
-      if (marker.displacementText != null &&
-          marker.displacementText!.isNotEmpty) {
-        lines.add('변위량: ${marker.displacementText}');
-      }
-      return lines;
-    }
-    if (builder != null) {
-      return builder(marker);
-    }
-    return [marker.label, marker.category.label];
-  }
-
-  String _formatNumber(double value) {
-    if (value % 1 == 0) {
-      return value.toStringAsFixed(0);
-    }
-    return value.toStringAsFixed(1);
-  }
-
   List<Widget> _buildMarkersForPage<T>({
     required Iterable<T> items,
     required int pageIndex,
@@ -1154,7 +1038,7 @@ class _DrawingScreenState extends State<DrawingScreen> {
       buildMarker: (defect) => DefectMarkerWidget(
         label: defect.label,
         category: defect.category,
-        color: _defectColor(defect.category),
+        color: defectColor(defect.category),
       ),
     );
   }
@@ -1172,7 +1056,7 @@ class _DrawingScreenState extends State<DrawingScreen> {
       buildMarker: (marker) => EquipmentMarkerWidget(
         label: marker.label,
         category: marker.category,
-        color: _equipmentColor(marker.category),
+        color: equipmentColor(marker.category),
       ),
     );
   }
@@ -1292,145 +1176,6 @@ class _DrawingScreenState extends State<DrawingScreen> {
     }
   }
 
-  Color _defectColor(DefectCategory category) {
-    switch (category) {
-      case DefectCategory.generalCrack:
-        return Colors.red;
-      case DefectCategory.waterLeakage:
-        return Colors.blue;
-      case DefectCategory.concreteSpalling:
-        return Colors.green;
-      case DefectCategory.other:
-        return Colors.purple;
-    }
-  }
-
-  String? _settlementDirection(EquipmentMarker marker) {
-    final direction = marker.tiltDirection;
-    if (direction != null && direction.isNotEmpty) {
-      return direction;
-    }
-    if (marker.equipmentTypeId == 'Lx' || marker.equipmentTypeId == 'Ly') {
-      return marker.equipmentTypeId;
-    }
-    return null;
-  }
-
-  int _nextSettlementIndex(String direction) {
-    return _site.equipmentMarkers
-            .where(
-              (marker) =>
-                  marker.category == EquipmentCategory.equipment8 &&
-                  _settlementDirection(marker) == direction,
-            )
-            .length +
-        1;
-  }
-
-  String _equipmentLabelPrefix(EquipmentCategory category) {
-    switch (category) {
-      case EquipmentCategory.equipment1:
-        return 'S';
-      case EquipmentCategory.equipment2:
-        return 'F';
-      case EquipmentCategory.equipment3:
-        return 'SH';
-      case EquipmentCategory.equipment4:
-        return 'Co';
-      case EquipmentCategory.equipment5:
-        return 'Ch';
-      case EquipmentCategory.equipment6:
-        return 'Tr';
-      case EquipmentCategory.equipment7:
-        return 'L';
-      case EquipmentCategory.equipment8:
-        return 'Lx';
-    }
-  }
-
-  String _equipmentDisplayLabel(EquipmentMarker marker) {
-    if (marker.category == EquipmentCategory.equipment8) {
-      return '부동침하 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'F') {
-      return '철근배근간격 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'SH') {
-      return '슈미트해머 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'Co') {
-      return '코어채취 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'Ch') {
-      return '콘크리트 탄산화 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'Tr') {
-      return '구조물 기울기 ${marker.label}';
-    }
-    if (marker.equipmentTypeId == 'L') {
-      return '부재처짐 ${marker.label}';
-    }
-    return marker.label;
-  }
-
-  Color _equipmentColor(EquipmentCategory category) {
-    switch (category) {
-      case EquipmentCategory.equipment1:
-        return Colors.pinkAccent;
-      case EquipmentCategory.equipment2:
-        return Colors.lightBlueAccent;
-      case EquipmentCategory.equipment3:
-      case EquipmentCategory.equipment4:
-        return Colors.green;
-      case EquipmentCategory.equipment5:
-        return Colors.orangeAccent;
-      case EquipmentCategory.equipment6:
-        return Colors.tealAccent;
-      case EquipmentCategory.equipment7:
-        return Colors.indigoAccent;
-      case EquipmentCategory.equipment8:
-        return Colors.deepPurpleAccent;
-    }
-  }
-
-  List<String> _defectTypeOptions(DefectCategory category) {
-    switch (category) {
-      case DefectCategory.generalCrack:
-        return StringsKo.defectTypesGeneralCrack;
-      case DefectCategory.waterLeakage:
-        return StringsKo.defectTypesWaterLeakage;
-      case DefectCategory.concreteSpalling:
-        return StringsKo.defectTypesConcreteSpalling;
-      case DefectCategory.other:
-        return StringsKo.defectTypesOther;
-    }
-  }
-
-  List<String> _defectCauseOptions(DefectCategory category) {
-    switch (category) {
-      case DefectCategory.generalCrack:
-        return StringsKo.defectCausesGeneralCrack;
-      case DefectCategory.waterLeakage:
-        return StringsKo.defectCausesWaterLeakage;
-      case DefectCategory.concreteSpalling:
-        return StringsKo.defectCausesConcreteSpalling;
-      case DefectCategory.other:
-        return StringsKo.defectCausesOther;
-    }
-  }
-
-  String _defectDialogTitle(DefectCategory category) {
-    switch (category) {
-      case DefectCategory.generalCrack:
-        return StringsKo.defectDetailsTitleGeneralCrack;
-      case DefectCategory.waterLeakage:
-        return StringsKo.defectDetailsTitleWaterLeakage;
-      case DefectCategory.concreteSpalling:
-        return StringsKo.defectDetailsTitleConcreteSpalling;
-      case DefectCategory.other:
-        return StringsKo.defectDetailsTitleOther;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
