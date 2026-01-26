1bdad5f917edf851f7835e4206201d9da6edaf93
dlrwns97 <dlrwns97@gmail.com>
2026-01-26 10:59:39 +0900

    Diet refactor: config-map equipment1~7 (no behavior change)

--- lib/screens/drawing/drawing_constants.dart
+++ lib/screens/drawing/drawing_constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safety_inspection_app/models/drawing_enums.dart';
 
 const Size DrawingCanvasSize = Size(1200, 1700);
 const double DrawingTapSlop = 8.0;
@@ -53,4 +54,71 @@ const Map<String, List<String>> DrawingEquipmentMemberSizeLabels = {
   '철골 L형강': ['A', 'B', 't'],
   '철골 C찬넬': ['A', 'B', 't'],
   '철골 H형강': ['H', 'B', 'tw', 'tf'],
+};
+
+class EquipmentFlowConfig {
+  const EquipmentFlowConfig({
+    required this.category,
+    required this.labelPrefix,
+    required this.dialogTitlePrefix,
+    required this.color,
+    this.displayLabelPrefix,
+  });
+
+  final EquipmentCategory category;
+  final String labelPrefix;
+  final String dialogTitlePrefix;
+  final String? displayLabelPrefix;
+  final Color color;
+}
+
+const Map<EquipmentCategory, EquipmentFlowConfig> DrawingEquipmentFlowConfigs = {
+  EquipmentCategory.equipment1: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment1,
+    labelPrefix: 'S',
+    dialogTitlePrefix: '부재단면치수',
+    color: Colors.pinkAccent,
+  ),
+  EquipmentCategory.equipment2: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment2,
+    labelPrefix: 'F',
+    dialogTitlePrefix: '철근배근간격',
+    displayLabelPrefix: '철근배근간격',
+    color: Colors.lightBlueAccent,
+  ),
+  EquipmentCategory.equipment3: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment3,
+    labelPrefix: 'SH',
+    dialogTitlePrefix: '슈미트해머',
+    displayLabelPrefix: '슈미트해머',
+    color: Colors.green,
+  ),
+  EquipmentCategory.equipment4: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment4,
+    labelPrefix: 'Co',
+    dialogTitlePrefix: '코어채취',
+    displayLabelPrefix: '코어채취',
+    color: Colors.green,
+  ),
+  EquipmentCategory.equipment5: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment5,
+    labelPrefix: 'Ch',
+    dialogTitlePrefix: '콘크리트 탄산화',
+    displayLabelPrefix: '콘크리트 탄산화',
+    color: Colors.orangeAccent,
+  ),
+  EquipmentCategory.equipment6: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment6,
+    labelPrefix: 'Tr',
+    dialogTitlePrefix: '구조물 기울기',
+    displayLabelPrefix: '구조물 기울기',
+    color: Colors.tealAccent,
+  ),
+  EquipmentCategory.equipment7: EquipmentFlowConfig(
+    category: EquipmentCategory.equipment7,
+    labelPrefix: 'L',
+    dialogTitlePrefix: '부재처짐',
+    displayLabelPrefix: '부재처짐',
+    color: Colors.indigoAccent,
+  ),
 };
