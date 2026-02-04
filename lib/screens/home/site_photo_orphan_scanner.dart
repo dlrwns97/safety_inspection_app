ecd8e13863df6203ec68bb2610ce532f667c63c1
dlrwns97 <dlrwns97@gmail.com>
2026-02-04 13:45:14 +0900

    Add site photo orphan scan dialog

---
+++ lib/screens/home/site_photo_orphan_scanner.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:safety_inspection_app/models/site.dart';
+
+class OrphanScanResult {
+  OrphanScanResult({required this.orphanFiles, required this.totalCount});
+
+  final List<FileSystemEntity> orphanFiles;
+  final int totalCount;
+
+  factory OrphanScanResult.empty() {
+    return OrphanScanResult(orphanFiles: [], totalCount: 0);
+  }
+}
+
+Future<OrphanScanResult> scanOrphanDefectPhotos({
+  required String siteId,
+  required Site site,
+}) async {
+  try {
+    final usedPaths = <String>{};
+    for (final defect in site.defects) {
+      for (final path in defect.details.photoPaths) {
+        if (path.isEmpty) {
+          continue;
+        }
+        usedPaths.add(_normalizePath(path));
+      }
+    }
+
+    final documentsDir = await getApplicationDocumentsDirectory();
+    final defectsDir = Directory('${documentsDir.path}/sites/$siteId/defects');
+    if (!await defectsDir.exists()) {
+      return OrphanScanResult.empty();
+    }
+
+    final entities = await defectsDir
+        .list(recursive: true, followLinks: false)
+        .toList();
+    final orphanFiles = <FileSystemEntity>[];
+    for (final entity in entities) {
+      if (entity is! File) {
+        continue;
+      }
+      final normalizedPath = _normalizePath(entity.path);
+      if (!usedPaths.contains(normalizedPath)) {
+        orphanFiles.add(entity);
+      }
+    }
+
+    return OrphanScanResult(
+      orphanFiles: orphanFiles,
+      totalCount: orphanFiles.length,
+    );
+  } catch (error) {
+    return OrphanScanResult.empty();
+  }
+}
+
+String _normalizePath(String path) {
+  return path.replaceAll('\\', '/');
+}
+
+String extractOrphanFileName(FileSystemEntity entity) {
+  final normalized = _normalizePath(entity.path);
+  final lastSlash = normalized.lastIndexOf('/');
+  if (lastSlash == -1) {
+    return normalized;
+  }
+  return normalized.substring(lastSlash + 1);
+}
+
+String? extractDefectIdFromPath({
+  required FileSystemEntity entity,
+  required String siteId,
+}) {
+  final normalized = _normalizePath(entity.path);
+  final marker = '/sites/$siteId/defects/';
+  final index = normalized.indexOf(marker);
+  if (index == -1) {
+    return null;
+  }
+  final remainder = normalized.substring(index + marker.length);
+  final segments = remainder.split('/');
+  if (segments.length < 2) {
+    return null;
+  }
+  return segments.first;
+}
